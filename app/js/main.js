9701c98cd9ca2d6ed4cb032ebfbbbbb5eb7753fe
Yulian <uliarx@gmail.com>
2021-03-03 22:13:42 +0200

    completed portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function(){
+    let mixer = mixitup('.portfolio__content');
     $('.reasons__slider-list').slick({
         autoplay: true,
         autoplaySpeed: 2000,
+    });
+    $('.module__slider').slick({
+        draggable: false,
+        autoplay: true,
     });
 });
